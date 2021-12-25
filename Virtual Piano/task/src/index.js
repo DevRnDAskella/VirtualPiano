1dd47c9432dd08d0f703f5d1895e5611005cbb6e
Alex Savelev <dev_rnd.askella@yahoo.com>
2021-12-25 12:53:22 +0300

    Modified: optimize code

--- Virtual Piano/task/src/index.js
+++ Virtual Piano/task/src/index.js
@@ -1,19 +1,24 @@
+const listWhiteKey = ['A', 'S', 'D', 'F', 'G', 'H', 'J'];
+const listBlackKey = ['W', 'E', 'T', 'Y', 'U'];
+const listGeneral = [...listWhiteKey, ...listBlackKey];
+
+let audioElement = null;
+
 document.addEventListener("keydown", evt => {
-    const listWhiteKey = ['A', 'S', 'D', 'F', 'G', 'H', 'J'];
-    const listBlackKey = ['W', 'E', 'T', 'Y', 'U'];
-    const listGeneral = [...listWhiteKey, ...listBlackKey];
     let code = evt.code.slice('key'.length);
 
     if (listGeneral.includes(code)) {
-        const audio = createAudioElement('./media/', code);
-        audio.play();
+        audioElement = createAudioElement('./media/', code);
+        audioElement.play();
     } else {
         console.log(`Warning`);
     }
 });
 
-function createAudioElement(path, key, format='mp3') {
+function createAudioElement(path, key, format = 'mp3') {
     const audio = document.createElement('AUDIO');
     audio.src = `${path}${key}.${format}`;
+    audio.type = `audio/${format}`;
+
     return audio;
 }
